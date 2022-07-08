56ed3a26b8302b69638d02ea805961cf928f9878
Sam Burrell <sam.burrell@signalmedia.co>
2022-07-08 13:10:36 +0100

    now namespace

--- lambda_code/main.go
+++ lambda_code/main.go
@@ -163,8 +163,8 @@ func handleAddSamples(valueType Values, value Value, timestamp int64) prompb.Sam
 
 func createMetricNameLabels(metricName string, namespace string, valueType Values, region string, account string) []*prompb.Label {
 	var labels []*prompb.Label
-	if !strings.HasPrefix(metricName, "AWS/") {
-		metricName = "aws_custom_" + metricName
+	if !strings.HasPrefix(namespace, "AWS/") {
+		metricName = "aws_custom_" + namespace
 	}
 	metricNameLabel := &prompb.Label{
 		Name:  "__name__",
