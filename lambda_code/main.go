10c178e48627326409b11dfb4a8411d95e5131df
Rahul De <rahul.de@signal-ai.com>
2022-07-11 13:51:34 +0100

    [signal-ai/sre-team/issues/191] rearrage a bit

--- lambda_code/main.go
+++ lambda_code/main.go
@@ -22,6 +22,13 @@ import (
 
 type Dimensions = map[string]string
 
+type Value struct {
+	Count float64 `json:"count"`
+	Sum   float64 `json:"sum"`
+	Max   float64 `json:"max"`
+	Min   float64 `json:"min"`
+}
+
 type MetricStreamData struct {
 	MetricStreamName string     `json:"metric_stream_name"`
 	AccountID        string     `json:"account_id"`
@@ -34,71 +41,14 @@ type MetricStreamData struct {
 	Unit             string     `json:"unit"`
 }
 
-type Value struct {
-	Count float64 `json:"count"`
-	Sum   float64 `json:"sum"`
-	Max   float64 `json:"max"`
-	Min   float64 `json:"min"`
-}
-
-type Values string
+type ValueType string
 
 const (
-	Count Values = "count"
-	Sum          = "sum"
-	Max          = "max"
-	Min          = "min"
+	Count ValueType = "count"
+	Sum             = "sum"
+	Max             = "max"
+	Min             = "min"
 )
-
-func HandleRequest(ctx context.Context, evnt events.KinesisFirehoseEvent) (events.KinesisFirehoseResponse, error) {
-	var response events.KinesisFirehoseResponse
-	var timeSeries []*prompb.TimeSeries
-	// These are the 4 value types from Cloudwatch, each of which map to a Prometheus Gauge
-	values := []Values{Count, Max, Min, Sum}
-
-	for _, record := range evnt.Records {
-		splitRecord := strings.Split(string(record.Data), string('\n'))
-
-		for _, x := range splitRecord {
-			// The Records includes an empty new line at the last position which becomes "" after parsing. Skipping over the empty string.
-			if x == "" {
-				continue
-			}
-			var metricStreamData MetricStreamData
-			json.Unmarshal([]byte(x), &metricStreamData)
-
-			// For each metric, the labels + valuetype is the __name__ of the sample, and the corresponding single sample value is used to create the timeseries.
-			for _, value := range values {
-				var samples []prompb.Sample
-				currentLabels := handleAddLabels(value, metricStreamData.MetricName, metricStreamData.Namespace, metricStreamData.Dimensions, metricStreamData.Region, metricStreamData.AccountID)
-				currentSamples := handleAddSamples(value, metricStreamData.Value, metricStreamData.Timestamp)
-				samples = append(samples, currentSamples)
-
-				singleTimeSeries := &prompb.TimeSeries{
-					Labels:  currentLabels,
-					Samples: samples,
-				}
-
-				timeSeries = append(timeSeries, singleTimeSeries)
-			}
-		}
-
-		// No transformation occurs, just send OK response back to Kinesis
-		var transformedRecord events.KinesisFirehoseResponseRecord
-		transformedRecord.RecordID = record.RecordID
-		transformedRecord.Result = events.KinesisFirehoseTransformedStateOk
-		transformedRecord.Data = []byte(string(record.Data))
-
-		response.Records = append(response.Records, transformedRecord)
-	}
-
-	err := createWriteRequestAndSendToAPS(timeSeries)
-	return response, err
-}
-
-func main() {
-	lambda.Start(HandleRequest)
-}
 
 // Taken directly from YACE: https://github.com/nerdswords/yet-another-cloudwatch-exporter/blob/1c7b3d7b7b64ce93bb4a27d8ef836e0c2b96b8e7/pkg/prometheus.go#L139
 func sanitize(text string) string {
@@ -118,19 +68,21 @@ func sanitize(text string) string {
 		">", "_",
 		"%", "_percent",
 	)
+
 	return replacer.Replace(text)
 }
 
 func toSnakeCase(str string) string {
 	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 
 	return strings.ToLower(snake)
 }
 
-func handleAddLabels(valueType Values, metricName string, namespace string, dimensions Dimensions, region string, account string) []*prompb.Label {
+func handleAddLabels(valueType ValueType, metricName string, namespace string, dimensions Dimensions, region string, account string) []*prompb.Label {
 	var labels []*prompb.Label
 
 	metricNameLabels := createMetricNameLabels(metricName, namespace, valueType, region, account)
@@ -141,7 +93,7 @@ func handleAddLabels(valueType Values, metricName string, namespace string, dime
 	return labels
 }
 
-func handleAddSamples(valueType Values, value Value, timestamp int64) prompb.Sample {
+func handleAddSamples(valueType ValueType, value Value, timestamp int64) prompb.Sample {
 	var val float64
 
 	switch valueType {
@@ -161,11 +113,13 @@ func handleAddSamples(valueType Values, value Value, timestamp int64) prompb.Sam
 	}
 }
 
-func createMetricNameLabels(metricName string, namespace string, valueType Values, region string, account string) []*prompb.Label {
+func createMetricNameLabels(metricName string, namespace string, valueType ValueType, region string, account string) []*prompb.Label {
 	var labels []*prompb.Label
+
 	if !strings.HasPrefix(namespace, "AWS/") {
 		namespace = "aws_custom_" + namespace
 	}
+
 	metricNameLabel := &prompb.Label{
 		Name:  "__name__",
 		Value: strings.ToLower(sanitize(namespace) + "_" + sanitize(toSnakeCase(metricName)) + "_" + sanitize(string(valueType))),
@@ -222,9 +176,8 @@ func encodeWriteRequestIntoProtoAndSnappy(writeRequest *prompb.WriteRequest) *by
 	}
 
 	encoded := snappy.Encode(nil, data)
-	body := bytes.NewReader(encoded)
 
-	return body
+	return bytes.NewReader(encoded)
 }
 
 func sendRequest(url string, bodyBytes []byte) error {
@@ -246,6 +199,7 @@ func sendRequest(url string, bodyBytes []byte) error {
 		}
 
 		bodyString := string(bodyBytes)
+
 		return fmt.Errorf("Error: statuscode %d sending data to endpoint: %s, %s", resp.StatusCode, url, bodyString)
 	}
 
@@ -269,4 +223,54 @@ func sendRequestToAPS(body *bytes.Reader) error {
 	}
 
 	return nil
+}
+
+func handleRequest(ctx context.Context, evnt events.KinesisFirehoseEvent) (events.KinesisFirehoseResponse, error) {
+	var response events.KinesisFirehoseResponse
+	var timeSeries []*prompb.TimeSeries
+	// These are the 4 value types from Cloudwatch, each of which map to a Prometheus Gauge
+	values := []ValueType{Count, Max, Min, Sum}
+
+	for _, record := range evnt.Records {
+		splitRecord := strings.Split(string(record.Data), "\n")
+
+		for _, x := range splitRecord {
+			// The Records includes an empty new line at the last position which becomes "" after parsing. Skipping over the empty string.
+			if x == "" {
+				continue
+			}
+
+			var metricStreamData MetricStreamData
+			json.Unmarshal([]byte(x), &metricStreamData)
+
+			// For each metric, the labels + valuetype is the __name__ of the sample, and the corresponding single sample value is used to create the timeseries.
+			for _, value := range values {
+				var samples []prompb.Sample
+				currentLabels := handleAddLabels(value, metricStreamData.MetricName, metricStreamData.Namespace, metricStreamData.Dimensions, metricStreamData.Region, metricStreamData.AccountID)
+				currentSamples := handleAddSamples(value, metricStreamData.Value, metricStreamData.Timestamp)
+				samples = append(samples, currentSamples)
+
+				singleTimeSeries := &prompb.TimeSeries{
+					Labels:  currentLabels,
+					Samples: samples,
+				}
+
+				timeSeries = append(timeSeries, singleTimeSeries)
+			}
+		}
+
+		// No transformation occurs, just send OK response back to Kinesis
+		var transformedRecord events.KinesisFirehoseResponseRecord
+		transformedRecord.RecordID = record.RecordID
+		transformedRecord.Result = events.KinesisFirehoseTransformedStateOk
+		transformedRecord.Data = record.Data
+
+		response.Records = append(response.Records, transformedRecord)
+	}
+
+	return response, createWriteRequestAndSendToAPS(timeSeries)
+}
+
+func main() {
+	lambda.Start(handleRequest)
 }
